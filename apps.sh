bb869cea2ff7bfd91fee1e171582806441c430b4
Peter Halliday <pghalliday@gmail.com>
2016-04-14 00:38:02 +0200

    refactoring

--- apps.sh
+++ apps.sh
@@ -2,31 +2,42 @@
 
 set -e
 
+function install-aur {
+  package=$1
+  if [ -d $package ]; then
+    cd $package
+    git pull
+  else
+    git clone https://aur.archlinux.org/$package.git
+    cd $package
+  fi
+  makepkg -si --noconfirm --needed
+  cd ..
+}
+
+function install {
+  yaourt -S --noconfirm --needed $1
+}
+
 # install yaourt
 sudo pacman -S --noconfirm --needed git
-git clone https://aur.archlinux.org/package-query.git
-cd package-query
-makepkg -si --noconfirm --needed
-cd ..
-git clone https://aur.archlinux.org/yaourt.git
-cd yaourt
-makepkg -si --noconfirm --needed
-cd ..
+install-aur package-query
+install-aur yaourt
 
 # install everything else
-yaourt -S --noconfirm --needed vim
-yaourt -S --noconfirm --needed tmux
-yaourt -S --noconfirm --needed openssh
-yaourt -S --noconfirm --needed xorg-server
-yaourt -S --noconfirm --needed xorg-server-utils
-yaourt -S --noconfirm --needed xorg-apps
-yaourt -S --noconfirm --needed xorg-xinit
-yaourt -S --noconfirm --needed compton
-yaourt -S --noconfirm --needed trayer
-yaourt -S --noconfirm --needed xmonad
-yaourt -S --noconfirm --needed ttf-dejavu
-yaourt -S --noconfirm --needed hsetroot
-yaourt -S --noconfirm --needed dmenu
-yaourt -S --noconfirm --needed xmobar
-yaourt -S --noconfirm --needed xscreensaver
-yaourt -S --noconfirm --needed rxvt-unicode-patched
+install vim
+install tmux
+install openssh
+install xorg-server
+install xorg-server-utils
+install xorg-apps
+install xorg-xinit
+install compton
+install trayer
+install xmonad
+install ttf-dejavu
+install hsetroot
+install dmenu
+install xmobar
+install xscreensaver
+install rxvt-unicode-patched
